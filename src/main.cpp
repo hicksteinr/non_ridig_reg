fe5b4e00041183efef9512deb4f4f9b8de5dbb5e
RichardHickstein <r.hickstein@gmx.de>
2025-05-09 12:44:22 +0200

    feat: script for single pseudo

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,38 @@
 #include <vector>
 #include "readers.hpp"
 
-igl::ARAPData arap_data;
+void run_arap(Eigen::MatrixXd &V, Eigen::MatrixXd &V_deformed, Eigen::MatrixXi &F, std::string &pseudo,
+              Eigen::VectorXi &indices, Eigen::MatrixXd &target_points, igl::ARAPData &arap_data) {
 
-int main(int argc, char* argv[]) {
+    // Initialize ARAP (3 = 3D)
+    arap_data.with_dynamics = false;
+    igl::arap_precomputation(V, F, 3, indices, arap_data);
 
-    std::string pseudo = "008";
+    // Solve ARAP
+    igl::arap_solve(target_points, arap_data, V_deformed);
+
+    igl::writeOBJ(
+        "/Users/richardhickstein/Documents/Development/Projects/LaFire/lafire_analysis_24/data/output/"
+        "tests/" +
+            pseudo + ".obj",
+        V_deformed, F);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "call with only single pseudo!" << std::endl;
+        return 1;
+    }
+    std::string pseudo = std::string(argv[1]);
+    igl::ARAPData arap_data;
 
     // read source mesh
-    vtkSmartPointer<vtkPolyData> mesh = readers::ReadPolyData(
-        ("/Users/richardhickstein/Documents/Development/Projects/LaFire/lafire_analysis_24/data/max_iir_meshes_clipped/"
-         "Baseline/" +
-         pseudo + ".vtk")
-            .c_str());
+    vtkSmartPointer<vtkPolyData> mesh =
+        readers::ReadPolyData(("/Users/richardhickstein/Documents/Development/Projects/LaFire/lafire_analysis_24/"
+                               "data/max_iir_meshes_clipped/"
+                               "Baseline/" +
+                               pseudo + ".vtk")
+                                  .c_str());
     vtkSmartPointer<vtkPoints> points = mesh->GetPoints();
     Eigen::MatrixXd V = Eigen::MatrixXd(static_cast<int>(points->GetNumberOfPoints()), 3);
     vtkSmartPointer<vtkCellArray> cells = mesh->GetPolys();
@@ -27,38 +47,29 @@ int main(int argc, char* argv[]) {
 
     Eigen::VectorXi indices;
     Eigen::MatrixXd target_points;
+
     readers::load_target_indices(
         "/Users/richardhickstein/Documents/Development/Projects/LaFire/lafire_analysis_24/data/output/"
-        "tests/" +
-            pseudo + "_additional_source_idxs_pvs.txt",
+        "correspondence_sets/geo_clip/" +
+            pseudo + "_lge_idxs.txt",
         indices);
     readers::load_target_points(
         "/Users/richardhickstein/Documents/Development/Projects/LaFire/lafire_analysis_24/data/output/"
-        "tests/" +
-            pseudo + "_additional_target_pts_pvs.txt",
+        "correspondence_sets/geo_clip/" +
+            pseudo + "_eam_pts.txt",
         target_points);
-
-    // readers::load_target_indices(
-    //     "/Users/richardhickstein/Documents/Development/Projects/LaFire/lafire_analysis_24/data/output/"
-    //     "correspondence_sets/pvs_only/lge_to_eam/" +
-    //         pseudo + "_source_idxs_pvs.txt",
-    //     indices);
-    // readers::load_target_points(
-    //     "/Users/richardhickstein/Documents/Development/Projects/LaFire/lafire_analysis_24/data/output/"
-    //     "correspondence_sets/pvs_only/lge_to_eam/" +
-    //         pseudo + "_target_pts_pvs.txt",
-    //     target_points);
+    std::cout << "processing case: " << pseudo << std::endl;
 
     Eigen::MatrixXd source_points(indices.size(), 3);
     for (int i = 0; i < indices.size(); ++i) {
         source_points.row(i) = V.row(indices[i]);
-        std::cout << "Index == " << indices[i] << std::endl << "V.row = " << V.row(indices[i]) << std::endl;
+        // std::cout << "Index == " << indices[i] << std::endl << "V.row = " << V.row(indices[i]) << std::endl;
     }
 
     // Plotting
     igl::opengl::glfw::Viewer viewer;
 
-    // Attach a menu plugin
+    // // Attach a menu plugin
     igl::opengl::glfw::imgui::ImGuiPlugin plugin;
     viewer.plugins.push_back(&plugin);
     igl::opengl::glfw::imgui::ImGuiMenu menu;
@@ -66,7 +77,7 @@ int main(int argc, char* argv[]) {
     // Customize the menu
     double doubleVariable = 0.1f;  // Shared between two menus
 
-    // Add content to the default menu window
+    // // Add content to the default menu window
     menu.callback_draw_viewer_menu = [&]() {
         // Draw parent menu content
         menu.draw_viewer_menu();
@@ -74,22 +85,9 @@ int main(int argc, char* argv[]) {
         // Add new group
         if (ImGui::CollapsingHeader("Custom-Menu", ImGuiTreeNodeFlags_DefaultOpen)) {
 
-            // Add a button
-            if (ImGui::Button("Load Mesh", ImVec2(-1, 0))) {
-                std::cout << "Hello\n";
-            }
             if (ImGui::Button("Run ARAP", ImVec2(-1, 0))) {
-                // Constraint: fix vertex 0 and move vertex 10 upward
-
-                // Simple 3D ARAP example with one fixed point
                 Eigen::MatrixXd V_deformed = V;
-
-                // Initialize ARAP (3 = 3D)
-                arap_data.with_dynamics = false;
-                igl::arap_precomputation(V, F, 3, indices, arap_data);
-
-                // Solve ARAP
-                igl::arap_solve(target_points, arap_data, V_deformed);
+                run_arap(V, V_deformed, F, pseudo, indices, target_points, arap_data);
 
                 // Update viewer
                 viewer.data().clear();
@@ -104,7 +102,7 @@ int main(int argc, char* argv[]) {
         }
     };
 
-    // Plot the mesh
+    // // Plot the mesh
     viewer.data().set_mesh(V, F);
     viewer.data().add_points(source_points, Eigen::RowVector3d(0, 0, 1));
     viewer.data().add_points(target_points, Eigen::RowVector3d(1, 0, 0));
